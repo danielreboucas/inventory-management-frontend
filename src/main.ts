1bc76e38ffb10845f409cc4edc2783ec297863bb
Daniel <danielreboucasq@gmail.com>
2024-04-01 19:54:41 -0300

    Added primevue

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+import PrimeVue from "primevue/config";
+import "primevue/resources/themes/aura-light-green/theme.css";
+
+const app = createApp(App);
+
+app.use(PrimeVue);
+app.use(store);
+app.use(router);
+app.mount("#app");
