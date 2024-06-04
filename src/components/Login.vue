fd26d141319b38a2678ea9e1091362c5a96f9440
Daniel <danielreboucasq@gmail.com>
2024-06-04 20:38:37 -0300

    style: changed Login and Signup screens layouts

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,31 +1,39 @@
 <template>
   <div class="login-container">
-    <div class="login">
-      <h2>Login</h2>
-      <div class="login-inputs">
-        <InputText v-model="email" placeholder="Email" />
-        <Password
-          v-model="password"
-          placeholder="Password"
-          toggleMask
-          :feedback="false"
-          @keydown.enter="requestSignInUser"
-        />
-      </div>
-
-      <div class="checkbox-container">
-        <div class="checkbox">
-          <Checkbox v-model="rememberMe" inputId="rememberme" :binary="true" />
-          <label for="rememberme">Remember me</label>
+    <div class="left"></div>
+    <div class="right">
+      <h1>Inventory Management</h1>
+      <div class="login">
+        <h2>Login</h2>
+        <div class="login-inputs">
+          <InputText v-model="email" placeholder="Email" />
+          <Password
+            v-model="password"
+            placeholder="Password"
+            toggleMask
+            :feedback="false"
+            @keydown.enter="requestSignInUser"
+          />
         </div>
-        <div class="forgot-password">Forgot password?</div>
-      </div>
 
-      <Button class="login-button" @click="requestSignInUser">Login</Button>
+        <div class="checkbox-container">
+          <div class="checkbox">
+            <Checkbox
+              v-model="rememberMe"
+              inputId="rememberme"
+              :binary="true"
+            />
+            <label for="rememberme">Remember me</label>
+          </div>
+          <div class="forgot-password">Forgot password?</div>
+        </div>
 
-      <div class="create-account">
-        Don't have an account?
-        <div class="signup" @click="moveToRegisterPage">Sign up here</div>
+        <Button class="login-button" @click="requestSignInUser">Login</Button>
+
+        <div class="create-account">
+          Don't have an account?
+          <div class="signup" @click="moveToRegisterPage">Sign up here</div>
+        </div>
       </div>
     </div>
   </div>
--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,29 +1,36 @@
 <template>
   <div class="register-container">
-    <div class="register">
-      <h2>Register</h2>
-      <div class="register-inputs">
-        <InputText v-model="firstName" placeholder="First name" />
-        <InputText v-model="lastName" placeholder="Last name" />
-        <InputText v-model="email" placeholder="Email name" />
-        <Password
-          v-model="password"
-          placeholder="Password"
-          toggleMask
-          :feedback="false"
-        />
-        <Password
-          v-model="passwordConfirm"
-          placeholder="Confirm password"
-          toggleMask
-          :feedback="false"
-        />
+    <div class="left">
+      <h1>Inventory Management</h1>
+      <div class="register">
+        <h2>Register</h2>
+        <div class="register-inputs">
+          <InputText v-model="firstName" placeholder="First name" />
+          <InputText v-model="lastName" placeholder="Last name" />
+          <InputText v-model="email" placeholder="Email name" />
+          <Password
+            v-model="password"
+            placeholder="Password"
+            toggleMask
+            :feedback="false"
+          />
+          <Password
+            v-model="passwordConfirm"
+            placeholder="Confirm password"
+            toggleMask
+            :feedback="false"
+          />
+        </div>
+        <Button class="register-button" @click="requestSignUpUser"
+          >Sign up</Button
+        >
+        <div class="user-signin">
+          Already have an account?
+          <span class="signin" @click="toSignIn">Sign In</span>
+        </div>
       </div>
-      <Button class="register-button" @click="requestSignUpUser"
-        >Sign up</Button
-      >
-      <span class="signin" @click="toSignIn">Sign In</span>
     </div>
+    <div class="right"></div>
   </div>
 </template>
 
